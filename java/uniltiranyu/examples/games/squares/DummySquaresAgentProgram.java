d326dec07b782482fdda9622ca374011b455188e
Jonatan Gomez <jogoper@gmail.com>
2021-04-26 13:03:14 -0500

    Refactor v 2.0

--- java/uniltiranyu/examples/games/squares/DummySquaresAgentProgram.java
+++ java/uniltiranyu/examples/games/squares/DummySquaresAgentProgram.java
@@ -12,27 +12,44 @@ import uniltiranyu.Percept;
 /**
  *
  * @author Jonatan
+ * Implements an AgentProgram
  */
 public class DummySquaresAgentProgram implements AgentProgram {
+    // Player's color
     protected String color;
+    // May use any structure as memory..
+    /**
+     * Creates a Dummy agent for playing squares
+     * @param color Player's color
+     */
     public DummySquaresAgentProgram( String color ){
         this.color = color;        
     }
     
     @Override
+    /**
+     * Computes an action to be carried by the agent according to the perception
+     * of the environment
+     * @param p Perception received by the agen program     * 
+     */
     public Action compute(Percept p) {
         long time = (long)(200 * Math.random());
         try{
            Thread.sleep(time);
         }catch(Exception e){}
+        // Determines if it is the agents turn
         if( p.get(Squares.TURN).equals(color) ){
+            // Gets the size of the board
             int size = Integer.parseInt((String)p.get(Squares.SIZE));
             int i = 0;
             int j = 0;
+            // Gets an square randomly and try to draw a border if possible
             Array<String> v = new Array<String>();
             while(v.size()==0){
               i = (int)(size*Math.random());
               j = (int)(size*Math.random());
+              // p.get(i+":"+j+":"+Squares.LEFT)
+              // gets "true" if the left border of square (i,j) is set, "false" otherwise
               if(((String)p.get(i+":"+j+":"+Squares.LEFT)).equals(Squares.FALSE))
                 v.add(Squares.LEFT);
               if(((String)p.get(i+":"+j+":"+Squares.TOP)).equals(Squares.FALSE))
@@ -44,6 +61,8 @@ public class DummySquaresAgentProgram implements AgentProgram {
             }
             try{
             	String move = v.get((int)(Math.random()*v.size()));
+            	// Action( i+":"+j+":"+Squares.BOTTOM ) 
+            	// draws bottom border of square (i,j)
                 return new Action( i+":"+j+":"+move);
             }catch(Exception e){}
         }
@@ -53,6 +72,4 @@ public class DummySquaresAgentProgram implements AgentProgram {
     @Override
     public void init() {
     }
-    
 }
-
